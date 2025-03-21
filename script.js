e0b886aeadf6af3b2554f9ffeefb2a5980835a33
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-21 23:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+document.getElementById('userForm').addEventListener('submit',function(event) {
+	event.preventDefault();
+	const name=document.getElementById('name').value;
+	const age=document.getElementById('age').value;
+	if(!name||!age){
+		alert('Please enter valid details.');
+		return;
+	}
+	const checkElegibility=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>=18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}else{
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		},4000);
+	});
+	checkElegibility
+		.then(message=>alert(message)).catch(message=>alert(message));
+});
+
